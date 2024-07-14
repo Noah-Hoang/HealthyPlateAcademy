18bee027a6e42120e68d57858e9d1efc0f393b4f
Je'Don Carter <jedon007@yahoo.com>
2024-07-14 10:57:02 -0700

    added ball code for basic scoreboard and restarts

---
+++ Assets/Scripts/PingPong/PingPongManager.cs
@@ -0,0 +1,34 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongManager : NetworkBehaviour
+{
+    public int score;
+    public GameObject ballPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        score += 1;
+
+        if (HasStateAuthority)
+        {
+
+            Runner.Despawn(ballPrefab);
+            Runner.Spawn(ballPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
