6d74714bf73a2aa7b3aca822e1c8c7c68c1825f9
Je'Don Carter <jedon007@yahoo.com>
2024-07-14 10:20:43 -0700

    Update NetworkedPaddle.cs

--- Assets/Scripts/PingPong/NetworkedPaddle.cs
+++ Assets/Scripts/PingPong/NetworkedPaddle.cs
@@ -20,30 +20,29 @@ public class NetworkedPaddle : NetworkBehaviour
         float moveX = 0f;
         float moveY = 0f;
 
-            Debug.Log("Capsule");
-            if (Input.GetKey(KeyCode.W))
-            {
-                Debug.Log("UP");
-                moveY = 0.1f * speed * Runner.DeltaTime;
-            }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("UP");
+            moveY = 0.1f * speed * Runner.DeltaTime;
+        }
 
-            if (Input.GetKey(KeyCode.S))
-            {
-                Debug.Log("DOWN");
-                moveY = -0.1f * speed * Runner.DeltaTime;
-            }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("DOWN");
+            moveY = -0.1f * speed * Runner.DeltaTime;
+        }
 
-            if (Input.GetKey(KeyCode.A))
-            {
-                Debug.Log("LEFT");
-                moveX = -0.1f * speed2 * Runner.DeltaTime;
-            }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("LEFT");
+            moveX = -0.1f * speed2 * Runner.DeltaTime;
+        }
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                Debug.Log("RIGHT");
-                moveX = 0.1f * speed2 * Runner.DeltaTime;
-            }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("RIGHT");
+            moveX = 0.1f * speed2 * Runner.DeltaTime;
+        }
 
         transform.Translate(0, moveY, 0);
         transform.Translate(moveX, 0, 0);
