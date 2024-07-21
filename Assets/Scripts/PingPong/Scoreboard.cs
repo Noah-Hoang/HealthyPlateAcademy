6a0f8f958ee6961948337ab429b5480a206ffb86
Je'Don Carter <jedon007@yahoo.com>
2024-07-21 10:22:52 -0700

    Update Scoreboard.cs

--- Assets/Scripts/PingPong/Scoreboard.cs
+++ Assets/Scripts/PingPong/Scoreboard.cs
@@ -9,40 +9,25 @@ public class Scoreboard : NetworkBehaviour
     public GameObject ballPrefab;
     public TMP_Text scoreText;
 
-    // Define the networked variable with a callback
-    [Networked]
-    public int networkedScore { get; set; }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-      
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //// The callback method that gets called when the variable changes
-    //private static void OnMyVariableChanged(Changed<MyNetworkedObject> changed)
-    //{
-    //    // Get the new value
-    //    int newValue = changed.Behaviour.MyVariable;
-    //    // Implement your custom logic here
-    //    Debug.Log($"MyVariable changed to: {newValue}");
-    //}
-
     private void OnTriggerEnter(Collider other)
     {
-        scoreText.text = networkedScore.ToString();
-
         if (HasStateAuthority)
         {
             networkedScore += 1;
             Runner.Despawn(other.gameObject.GetComponent<NetworkObject>());
             Runner.Spawn(ballPrefab, Vector3.zero, Quaternion.identity);
         }
+    }
+
+
+    // Define the networked variable with a callback
+    [Networked, OnChangedRender(nameof(OnScoreChanged))]
+    public int networkedScore { get; set; }
+
+    // The callback method that gets called when the variable changes
+    private void OnScoreChanged()
+    {
+        Debug.Log("Score changed to: " + networkedScore);
+        scoreText.text = networkedScore.ToString();
     }
 }
