8ddff0b49645b9e74328f55242869272529cc8d7
0shakey <118243453+0shakey@users.noreply.github.com>
2024-07-21 09:28:00 -0700

    Added Scoreboard script

---
+++ Assets/Scripts/PingPong/Scoreboard.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    public int playerOneScore;
+    public int playerTwoScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            
+    }
+}
