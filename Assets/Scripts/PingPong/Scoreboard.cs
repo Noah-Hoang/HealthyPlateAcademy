4d8276e1f07d8ec55219a7d3d8e5029a22125006
Je'Don Carter <jedon007@yahoo.com>
2024-07-21 09:48:11 -0700

    Update Scoreboard.cs

--- Assets/Scripts/PingPong/Scoreboard.cs
+++ Assets/Scripts/PingPong/Scoreboard.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
-public class Scoreboard : MonoBehaviour
+public class Scoreboard : NetworkBehaviour
 {
     public int score;
+
+    // Define the networked variable with a callback
+    [Networked]
+    public int networkedScore { get; set; }
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +23,18 @@ public class Scoreboard : MonoBehaviour
         
     }
 
+    //// The callback method that gets called when the variable changes
+    //private static void OnMyVariableChanged(Changed<MyNetworkedObject> changed)
+    //{
+    //    // Get the new value
+    //    int newValue = changed.Behaviour.MyVariable;
+    //    // Implement your custom logic here
+    //    Debug.Log($"MyVariable changed to: {newValue}");
+    //}
+
     private void OnTriggerEnter(Collider other)
     {
         score += 1;
+        networkedScore += 1;
     }
 }
