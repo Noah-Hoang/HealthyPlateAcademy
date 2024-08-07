8921815119ef0659443a72d26f81c49bcb0c2340
0shakey <118243453+0shakey@users.noreply.github.com>
2024-08-06 17:46:00 -0700

    Update NetworkedGrab.cs

--- Assets/Scripts/HealthyPlate/NetworkedGrab.cs
+++ Assets/Scripts/HealthyPlate/NetworkedGrab.cs
@@ -2,28 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NetworkedGrab : NetworkBehaviour, IStateAuthorityChanged
 {
     [Networked, OnChangedRender(nameof(OnHeldChanged))]
     public bool onHeld { get; set; }
+    public XRGrabInteractable interactable;
 
     public void OnHeldChanged()
     {
-
+        if (Object.HasStateAuthority)
+        {
+            return;
+        }
+        if (onHeld == true)
+        {
+            interactable.enabled = false;
+        }
+        else if (onHeld == false)
+        {
+            interactable.enabled = true;
+        }
     }
 
     public void StateAuthorityChanged()
     {
         Debug.Log("State Authority Changed to " + Runner.LocalPlayer.PlayerId);
-    }
-
-    public void CheckIfHeld()
-    {
-        if (onHeld)
-        {
-           //stop from being grabbed by another person   
-        }
     }
 
     public void OnGrab()
