2abee440a242fd71827180ef0ce3bfe0d11606ba
Je'Don Carter <jedon007@yahoo.com>
2024-08-30 19:35:42 -0700

    removed extra code

--- Assets/Scripts/HealthyPlate/ChefKnife.cs
+++ Assets/Scripts/HealthyPlate/ChefKnife.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class ChefKnife : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.root.gameObject.tag == "Ingredient")
