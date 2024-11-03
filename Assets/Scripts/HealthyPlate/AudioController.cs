38bf4394960c8953231c9859d68ee9215bb1013c
0shakey <118243453+0shakey@users.noreply.github.com>
2024-11-03 11:02:14 -0800

    added audio

---
+++ Assets/Scripts/HealthyPlate/AudioController.cs
@@ -0,0 +1,82 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class AudioController : MonoBehaviour
+{
+
+    public UnityEvent onSoundEffectStarted;
+    public UnityEvent onSoundEffectEnded;
+
+    public virtual void OnEnable()
+    {       
+        onSoundEffectStarted.AddListener(StartSoundEffectCallback);
+        onSoundEffectEnded.AddListener(StopSoundEffectCallback);
+    }
+
+    public virtual void OnDisable()
+    {        
+        onSoundEffectStarted.RemoveListener(StartSoundEffectCallback);
+        onSoundEffectEnded.RemoveListener(StopSoundEffectCallback);
+    }
+
+    [Rpc]
+    public void StartSoundEffectRPC()
+    {
+        StartSoundEffect();
+    }
+
+    public void StartSoundEffect()
+    {
+        onSoundEffectStarted.Invoke();
+    }
+
+    public void StartSoundEffectCallback()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            // Gets the soundEffect system from the children
+           AudioSource soundEffect = transform.GetChild(i).GetComponent<AudioSource>();
+
+            // Checks if the child has a soundEffect system attached
+            if (soundEffect == null)
+            {
+                continue;
+            }
+
+            soundEffect.Stop();  // Stop it first to ensure it restarts from the beginning                
+            soundEffect.Play();  // Play the soundEffect effect 
+        }
+    }
+
+    [Rpc]
+    public void StopSoundEffectRPC()
+    {
+        StopSoundEffect();
+    }
+
+    public void StopSoundEffect()
+    {
+        onSoundEffectEnded.Invoke();
+    }
+
+    public void StopSoundEffectCallback()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            // Gets the soundEffect system from the children
+            AudioSource soundEffect = transform.GetChild(i).GetComponent<AudioSource>();
+
+            // Checks if the child has a soundEffect system attached
+            if (soundEffect == null)
+            {
+                continue;
+            }
+
+            soundEffect.Stop();  // Stop it first to ensure it restarts from the beginning
+        }
+    }
+}
