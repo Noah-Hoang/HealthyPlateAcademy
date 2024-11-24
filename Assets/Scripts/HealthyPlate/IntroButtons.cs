d4cb4224e7eacee58d64b9366b7cc12fb16f5311
0shakey <118243453+0shakey@users.noreply.github.com>
2024-11-24 10:20:53 -0800

    Added IntroScene and SplashScreen. Added scenes to BuildSettings and coded timer to transition SplashScreen to IntroScene

---
+++ Assets/Scripts/HealthyPlate/IntroButtons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroButtons : MonoBehaviour
+{
+    public void StandardMode()
+    {
+        Debug.Log("Play Standard Mode");
+    }
+
+    public void InstructorMode()
+    {
+        Debug.Log("Play Instructor Mode");
+    }
+}
---
+++ Assets/Scripts/HealthyPlate/SplashScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ScreenTimer());
+    }
+
+    IEnumerator ScreenTimer()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("IntroScene");
+    }
+}
