360c68771346e8dbb5fff923a37e138b003499b6
0shakey <118243453+0shakey@users.noreply.github.com>
2025-01-05 10:22:56 -0800

    Added networked name display

--- Assets/Scripts/HealthyPlate/CharacterNameDisplay.cs
+++ Assets/Scripts/HealthyPlate/CharacterNameDisplay.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Fusion;
 
-public class CharacterNameDisplay : MonoBehaviour
+public class CharacterNameDisplay : NetworkBehaviour
 {
     public TextMeshProUGUI nameDisplay;
 
-    public void Start()
+    [Networked, OnChangedRender(nameof(SetNameDisplay))]
+    public string characterName { get; set; }
+
+    public override void Spawned()
     {
-        nameDisplay.text = IntroButtons.characterName;
+        if (Object.HasStateAuthority)
+        {
+            characterName = IntroButtons.characterName;
+        }
+    }
+
+    public void SetNameDisplay()
+    {
+        nameDisplay.text = characterName;
     }
 }
