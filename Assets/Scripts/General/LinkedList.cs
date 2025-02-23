2dfdd4d4f382843397d369cf80e6eb3a8f43a437
0shakey <118243453+0shakey@users.noreply.github.com>
2025-02-23 10:59:32 -0800

    Linked List

--- Assets/Scripts/General/LinkedList.cs
+++ Assets/Scripts/General/LinkedList.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class LinkedList 
+public class LinkedList : MonoBehaviour
 {
     private Node head; //First node in the linked list
+    public int linkedListLength;
 
     public void Add(int value)
     {
@@ -21,6 +23,7 @@ public class LinkedList
         if (head == null)
         {
             head = newNode;
+            linkedListLength++;
         }
         else
         {
@@ -30,6 +33,64 @@ public class LinkedList
                 currentNode = currentNode.nextNode;
             }
             currentNode.nextNode = newNode;
+            linkedListLength++;
+        }
+    }
+
+    public void Start()
+    {
+        MakeBurger(2, true, 2, true);
+        Node tempNode = new Node(0);
+        AddAt(2);
+        AddAt(2, tempNode);
+        OrderBurger("Cheese");
+        OrderBurger();
+    }
+
+    public void MakeBurger(int patties, bool hasCheese = false, int pickles = 2, bool useLettuce = false)
+    {
+
+    }
+    public void OrderBurger(string extraTopping = "none")
+    {
+        Console.WriteLine("You ordered a burger with " + extraTopping);
+    }
+    public void AddAt(int index, Node newNode = null)
+    {
+        Node currentNode = head;
+        int tempIndex = 0;
+
+        if (newNode == null)
+        {
+            newNode = new Node(0);
+        }
+
+        if (index == 0)
+        {
+            newNode.nextNode = currentNode;
+            head = newNode;
+            return;
+        }
+
+        // 2 4 3 5
+        while (tempIndex <= index && (linkedListLength > index))
+        {
+            if (tempIndex + 1 == index)
+            {
+                if (currentNode.nextNode.nextNode != null)
+                {
+                    newNode.nextNode = currentNode.nextNode.nextNode;
+                    currentNode.nextNode = newNode;
+                }
+                else
+                {
+                    newNode.nextNode = currentNode.nextNode;
+                    currentNode.nextNode = newNode;
+                }
+                return;
+            }
+            currentNode = currentNode.nextNode;
+            tempIndex++;
         }
     }
     
@@ -52,21 +113,35 @@ public class LinkedList
         if (index == 0)
         {
             head = currentNode.nextNode;
+            linkedListLength--;
         }
 
-        //TODO: Check for nulls and index being bigger than linked list
-        // -3 1 5
+        //TODO: Check to see if the next next node exists
+        // Check to see if index is bigger than linked list
+        // -3 1 5 9 
         int tempIndex = 0;
-        while (tempIndex <= index)
+        
+        while (tempIndex <= index && (linkedListLength > index))
         {
             if (tempIndex + 1 == index)
             {
-                currentNode.nextNode = currentNode.nextNode.nextNode;
-                return;
+                if (currentNode.nextNode.nextNode == null)
+                {
+                    currentNode.nextNode = null;
+                    linkedListLength--;
+                    return;
+                }
+                else
+                {
+                    currentNode.nextNode = currentNode.nextNode.nextNode;
+                    linkedListLength--;
+                    return;
+                }
             }
             currentNode = currentNode.nextNode;
             tempIndex++;
         }
+        
     }
 
     public void RemoveValue(int value)
--- Assets/Scripts/General/Node.cs
+++ Assets/Scripts/General/Node.cs
@@ -13,4 +13,6 @@ public class Node
         value = tempValue;
         nextNode = tempNextNode;
     }
+
+    
 }
