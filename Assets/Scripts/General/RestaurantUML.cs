2b2b6f3e9595b910d8b8abab5982629ae4667163
0shakey <118243453+0shakey@users.noreply.github.com>
2025-03-02 11:03:30 -0800

    Created RestaurantUML

---
+++ Assets/Scripts/General/RestaurantUML.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestaurantUML : MonoBehaviour
+{
+    //Restaryant singleton
+    /*
+     * Pays employees
+     * Has relevant references
+    */
+    // Staff class that is inherited by individual staff
+    /*
+     * Working hours
+     * Variable for wage
+    */
+    // Waiters
+    /*
+     * Take orders (also custom orders)
+     * Serve Food
+     * Promote specials/items
+     * Takes money to cash register
+     * If tipped, take money
+    */
+    //Busboys
+    /*
+     * Clears table when customers are done
+     * Takes dishes to kitchen to clean
+    */
+    //Chefs
+    /*
+     * Queue of orders
+     * Prepare food
+    */
+    //Host
+    /*
+     * Takes customers to clean tables
+     * Take into account party size and table size
+     * If tables are not available, puts cutomers on hold
+    */
+    //Menu Class
+    /*
+     * Orders have different cook times (customers don't know that)
+     * Menu has list of food items 
+     * List of substitutions for foods
+     * Different lists for different sections on a menu + kids section
+     * Default parameters for food items for customization
+     * Constructor for food items (food, side, cusomization)
+     * Randomly choose 2 items to be specials for the day
+    */
+    //Customer Class
+    /*
+     * Party size vairable
+     * Place orders
+     * Pays and leaves tip
+    */
+    //Cash Register Class
+    /*
+     * Takes money
+    */
+    //Table
+    /*
+     * Clean or dirty bool
+     * Table size
+    */
+    //Kitchen class
+    /*
+     * 
+    */
+}
