ae45da3e4ae60b3c93b6beeadbb1941d4512659d
0shakey <118243453+0shakey@users.noreply.github.com>
2025-03-14 18:59:35 -0700

    Practice scripts

--- Assets/Scripts/General/TestScript_1.cs
+++ Assets/Scripts/General/TestScript_1.cs
@@ -7,13 +7,24 @@ public class TestScript_1 : MonoBehaviour
     public static int testInt;
     void Start()
     {
-        int i = 0;
-       
+        Add(3, 2);
+        Add(3.0f, 2.0f);
+        Add(3.0f, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public float Add(float first, float second)
+    {
+        return first + second;
+    }
+
+    public int Add(int first, int second)
+    {
+        return first + second;
     }
 }
